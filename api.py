b71d055d8c57d094ac83f1a90642d0c18c2ec78a
Jakub Piskiewicz <jakub@piskiewicz.org>
2022-09-16 19:04:31 +0200

    Removed quotes

--- api.py
+++ api.py
@@ -154,7 +154,7 @@ def newp():
         print(post, flush=True)
         timeFormat = "%d.%m.%Y %H:%M"
         payloadT = "STR_TO_DATE('%s', '%s')" % (post["date"], timeFormat.replace("M", "i"))
-        payload = "%d, %d, '%s', '%s', '%s', '%s'" % (post["id"], post["image"], post["title"], post["description"], payloadT, post["content"])
+        payload = "%d, %d, '%s', '%s', %s, '%s'" % (post["id"], post["image"], post["title"], post["description"], payloadT, post["content"])
         query = "INSERT INTO posts (id, image, title, description, date, content) VALUES (%s)" % (payload)
         execQuery(query)
         for tag in post["tags"]:
